a32a6b9ebe5d673787fa5ca5a2243294f298427d
Hibatullah Syauqi At Taqi <hibatullah_syauqi@hotmail.com>
2025-05-01 13:09:32 +0700

    initialize project, include .idea/ folder into .gitignore file

---
+++ src/main/java/com/bootcamp/handsonspringbootdemo/HandsonSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.handsonspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsonSpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandsonSpringbootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcamp/handsonspringbootdemo/HandsonSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.handsonspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsonSpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
